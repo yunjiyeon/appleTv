7ea4b7e6549d26e0c4ff4a9b95328d8bcf111fd3
yunjiyeon <yangdom@naver.com>
2023-01-03 12:27:50 +0900

    movie 수정

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Movies = () => {
-  return <div></div>;
+  return (
+    <div className="info-text">
+      <h1>진행 중입니다 😊</h1>
+      <p>조금만 기다려 주세요</p>
+    </div>
+  );
 };
 
 export default Movies;
